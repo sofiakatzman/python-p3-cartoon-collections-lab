ac08b5454d605fed0e0a6f2f788185478cc76833
Sofia Katzman <katzmansof@gmail.com>
2023-05-21 01:18:12 -0400

    finished lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
+def roll_call_dwarves(self):
+    for i in range(len(self)):
+        print(f"{i + 1}. {self[i]}")
     pass
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planteers):
+    new_planteers = []
+    for planteer in planteers:
+        new_planteer = planteer.capitalize() + "!"
+        new_planteers.append(new_planteer)
+    return new_planteers
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(strings):
+    cheeses = ["cheddar", "gouda", "camembert"]
+    for string in strings:
+        if string in cheeses:
+            return string
+
+    return None
